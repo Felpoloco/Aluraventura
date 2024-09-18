91391600de4e7b6a6c1363fb0bc841e770a50413
Felpszz <00001118334668sp@al.educacao.sp.gov.br>
2024-09-18 15:26:22 -0300

    Adicionando assinatura no js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
+    //FELIPE DA SILVA SANTOS 3ºD /18/09/2024/
     button.addEventListener('click', function(){
         const atual = document.querySelector('.ativo');
         const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
